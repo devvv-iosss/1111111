22bbcfbb2e3e6238e2c09d7cdb866dc6a2bafea2
ios_dev <futureiosdeveloper@gmail.com>
2021-10-13 18:03:49 +0300

    Initial Commit

---
+++ 1111111/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1111111
+//
+//  Created by Vladimir Syleimanov on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
